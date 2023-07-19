1c48b4b5bead41bac51d5d2f018167e9d5a4b7da
vikas <vikas@LAPTOP-1RGMJ9DD>
2023-07-19 10:20:15 +0530

    Source code

---
+++ jenkinsdemo/src/test/java/jenkinsdemo/VerifyTitle.java
@@ -0,0 +1,37 @@
+package jenkinsdemo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class VerifyTitle {
+WebDriver driver;
+	
+	@BeforeTest
+	public void launchApplication()
+	{
+    driver=new ChromeDriver();
+		
+		//2) Navigate to application
+		// object.method()
+		driver.get("https://facebook.com");
+		
+	}
+	@Test
+	public void titleVerification()
+	{
+		String expectedTitle="Facebook – log in or sign up";
+		String actualTitle=driver.getTitle();
+		Assert.assertEquals(actualTitle, expectedTitle);
+		
+	}
+	@AfterTest
+	
+	public void closeBrowser()
+	{
+		driver.quit();
+	}
+}
