24017be0f4a8550f76e44f3c56ae2a9a5116f599
vikas <vikas@LAPTOP-1RGMJ9DD>
2023-07-20 10:40:56 +0530

    Updated code

--- jenkinsdemo/src/test/java/jenkinsdemo/VerifyTitleTest.java
+++ jenkinsdemo/src/test/java/jenkinsdemo/VerifyTitleTest.java
@@ -2,19 +2,31 @@ package jenkinsdemo;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class VerifyTitle {
+public class VerifyTitleTest {
 WebDriver driver;
 	
 	@BeforeTest
 	public void launchApplication()
+	
 	{
-    driver=new ChromeDriver();
 		
+		String os = System.getProperty("os.name").toLowerCase();
+		if (os.contains("win")){			
+			driver = new ChromeDriver();
+		}
+		    
+		else if (os.contains("nix") || os.contains("aix") || os.contains("nux")){
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--headless");			
+			driver = new ChromeDriver(options);
+
+		}
 		//2) Navigate to application
 		// object.method()
 		driver.get("https://facebook.com");
